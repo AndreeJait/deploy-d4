b3ea3d098fa3e8df8bea370cbfac63d630e3f4cf
Andree Panjaitan <andreepanjaitan@Andrees-MacBook-Pro.local>
2023-05-03 23:27:36 +0700

    update

--- ta-10/staticfiles/scanning/detail.js
+++
@@ -1,9 +0,0 @@
-let dataString = localStorage.getItem("last-result");
-let data = JSON.parse(dataString).data
-console.log(data);
-console.log(data.size);
-let inputPrice = document.getElementById("price");
-let inputVolume = document.getElementById("volume");
-
-inputPrice.value = data.price;
-inputVolume.value = data.size.volume.toFixed(2) + " mL";
